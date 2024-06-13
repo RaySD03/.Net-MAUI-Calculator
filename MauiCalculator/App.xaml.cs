c79efab574156004fd0608f65e4e71329577ed9d
RaySD03 <aniksayad@yahoo.com>
2024-06-12 22:09:29 -0700

    Added default window dimensions.

--- MauiCalculator/App.xaml.cs
+++ MauiCalculator/App.xaml.cs
@@ -7,8 +7,13 @@ public partial class App : Application
 	public App()
 	{
 		InitializeComponent();
-
-		//MainPage = new AppShell();
 		MainPage = new NavigationPage(new MainPage());
+    }
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        var window = base.CreateWindow(activationState);
+        window.Width = 700;
+        window.Height = 510;
+        return window;
     }
 }
